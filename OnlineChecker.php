3833da8d5b399b1204f154a77b6318af122bf474
Fermín Olaiz <ferminolaiz@gmail.com>
2014-07-24 14:39:56 -0300

    Second beta
    + MultiSender
    + Avoid send warning repeatedly
    + Fixed http code logical comparer error

--- OnlineChecker.php
+++ OnlineChecker.php
@@ -2,8 +2,8 @@
 	try
 	{
 		### START CONFIG ###
-		$URL = 'http://github.com/ferminolaiz/url_that_not_exists'; // URL to check
-		$To = ''; // Number to send the error report
+		$URL = 'http://github.com/ferminolaiz/url_that_not_exists';
+		$Numbers = Array('');
 		
 		$WP_Username = '';
 		$WP_Password = '';
@@ -13,25 +13,44 @@
 		
 		require_once('api/whatsprot.class.php');
 		
+		$Sended = file_get_contents('issended.dat');
+		
 		$Headers = get_headers($URL, 1);
 		
-		$Data = Array();
-		
-		if($Headers[0] != 'HTTP/1.0 200 OK' || $Headers[0] != 'HTTP/1.1 200 OK')
+		if($Headers[0] != 'HTTP/1.0 200 OK' && $Headers[0] != 'HTTP/1.1 200 OK')
 		{
 			$M = "Underc0de Online Checker - by fermino - http://underc0de.org/fermino\r\n\r\n";
-			
+				
 			$M .= "Error in " . $URL . "\r\n\r\n";
-			
+				
 			$M .= "Error code: " . $Headers[0] . "\r\n";
 			$M .= "Date: " . date('d/m/Y H:i:s') . "\r\n";
 			
-			$w = new WhatsProt($WP_Username, $WP_Identity, $WP_Nickname);
-			$w->connect();
-			
-			$w->loginWithPassword($WP_Password);
-			
-			$w->sendMessage($To, $M);
+			if($Sended == 'false')
+			{
+				$w = new WhatsProt($WP_Username, $WP_Identity, $WP_Nickname);
+				$w->connect();
+				
+				$w->loginWithPassword($WP_Password);
+				
+				foreach($Numbers as $To)
+				{
+					$w->sendMessage($To, $M);
+				}
+				
+				$File = fopen('issended.dat', 'w');
+				fwrite($File, 'true');
+				fclose($File);
+			}
+		}
+		else
+		{
+			if($Sended == 'true')
+			{
+				$File = fopen('issended.dat', 'w');
+				fwrite($File, 'false');
+				fclose($File);
+			}
 		}
 	}
 	catch (Exception $E)
