73639215b1ba2854f57840f0560d0e7f71c39efd
Fermín Olaiz <ferminolaiz@gmail.com>
2014-08-06 09:03:24 -0300

    Added protection to not found hostname

--- OnlineChecker.php
+++ OnlineChecker.php
@@ -22,53 +22,60 @@
 			
 			$Headers = get_headers($URL, 1); // Get $URL headers
 			
-			$w = new WhatsProt($WP_Username, $WP_Identity, $WP_Nickname); // Load an instance of WhatsApi class
-			$w->connect(); // Connect WhatsApi to whatsapp server
-				
-			$w->loginWithPassword($WP_Password); // Login in Whatsapp servers
-			
-			if($Headers[0] != 'HTTP/1.0 200 OK' && $Headers[0] != 'HTTP/1.1 200 OK') // If URL's http code isn't 200 (OK) ==>
+			if($Headers != false)
 			{
-				if($Sended == 'false') // If notification state is false ==>
+				$w = new WhatsProt($WP_Username, $WP_Identity, $WP_Nickname); // Load an instance of WhatsApi class
+				$w->connect(); // Connect WhatsApi to whatsapp server
+					
+				$w->loginWithPassword($WP_Password); // Login in Whatsapp servers
+				
+				if($Headers[0] != 'HTTP/1.0 200 OK' && $Headers[0] != 'HTTP/1.1 200 OK') // If URL's http code isn't 200 (OK) ==>
 				{
-					$M = "Underc0de Online Checker - by fermino - http://underc0de.org/profile/fermino\r\n\r\n"; // Generate message
-					
-					$M .= "Error in " . $URL . "\r\n\r\n"; // Generate message
-					
-					$M .= "Error code: " . $Headers[0] . "\r\n"; // Generate message
-					$M .= "Date: " . date('d/m/Y H:i:s'); // Generate message
-					
-					foreach($Numbers as $To) // Numbers to send message
+					if($Sended == 'false') // If notification state is false ==>
 					{
-						$w->sendMessage($To, $M); // Send message to number
+						$M = "Underc0de Online Checker - by fermino - http://underc0de.org/profile/fermino\r\n\r\n"; // Generate message
+						
+						$M .= "Error in " . $URL . "\r\n\r\n"; // Generate message
+						
+						$M .= "Error code: " . $Headers[0] . "\r\n"; // Generate message
+						$M .= "Date: " . date('d/m/Y H:i:s'); // Generate message
+						
+						foreach($Numbers as $To) // Numbers to send message
+						{
+							$w->sendMessage($To, $M); // Send message to number
+						}
+						
+						echo $M; // Show the error (Cron Job's log)
+						
+						$File = fopen('issended.dat', 'w'); // Open the notification state file
+						fwrite($File, 'true'); // Save state
+						fclose($File); // Close the fle
 					}
-					
-					echo $M; // Show the error (Cron Job's log)
-					
-					$File = fopen('issended.dat', 'w'); // Open the notification state file
-					fwrite($File, 'true'); // Save state
-					fclose($File); // Close the fle
+				}
+				else // If headers are 200 (OK)
+				{
+					if($OKPrint) echo 'Everything is OK in ' . $URL; // Show 'Everything is OK' message (only if OC isn't running in cron [$OKPrint]) 
+					if($Sended == 'true') // If notification state is true (This will send a 'Site is back online' notification)
+					{
+						$M = "Underc0de Online Checker - by fermino - http://underc0de.org/profile/fermino\r\n\r\n"; // Generate message
+						
+						$M .= $URL . " back online\r\n\r\n"; // Generate message
+						$M .= "Date: " . date('d/m/Y H:i:s'); // Generate message
+						
+						foreach($Numbers as $To) // Numbers to send message
+						{
+							$w->sendMessage($To, $M); // Send message to number
+						}
+						
+						$File = fopen('issended.dat', 'w'); // Open the notification state file
+						fwrite($File, 'false'); // Save state
+						fclose($File); // Close file
+					}
 				}
 			}
-			else // If headers are 200 (OK)
+			else
 			{
-				if($OKPrint) echo 'Everything is OK in ' . $URL; // Show 'Everything is OK' message (only if OC isn't running in cron [$OKPrint]) 
-				if($Sended == 'true') // If notification state is true (This will send a 'Site is back online' notification)
-				{
-					$M = "Underc0de Online Checker - by fermino - http://underc0de.org/profile/fermino\r\n\r\n"; // Generate message
-					
-					$M .= $URL . " back online\r\n\r\n"; // Generate message
-					$M .= "Date: " . date('d/m/Y H:i:s'); // Generate message
-					
-					foreach($Numbers as $To) // Numbers to send message
-					{
-						$w->sendMessage($To, $M); // Send message to number
-					}
-					
-					$File = fopen('issended.dat', 'w'); // Open the notification state file
-					fwrite($File, 'false'); // Save state
-					fclose($File); // Close file
-				}
+				if($OKPrint) echo 'Can\'t resolve hostname';
 			}
 		}
 		else // If OC isn't enabled
